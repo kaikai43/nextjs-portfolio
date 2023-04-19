2ac1aeacefe00321044ce1ca7da8c907eed33085
Jin Kai Teh <jinkai4393@gmail.com>
2023-04-19 20:16:53 +1000

    Remove template info

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,18 +14,18 @@ const kaisei = localFont({
 
 export const metadata: Metadata = {
   title: {
-    default: 'Lee Robinson',
-    template: '%s | Lee Robinson',
+    default: 'Jin Kai Teh',
+    template: '%s | Jin Kai Teh',
   },
   description: 'Developer, writer, and creator.',
   openGraph: {
-    title: 'Lee Robinson',
+    title: 'Jin Kai Teh',
     description: 'Developer, writer, and creator.',
-    url: 'https://leerob.io',
-    siteName: 'Lee Robinson',
+    url: 'https://jinkt.dev',
+    siteName: 'Jin Kai Teh',
     images: [
       {
-        url: 'https://leerob.io/og.jpg',
+        url: 'https://jinkt.dev/og.jpg',
         width: 1920,
         height: 1080,
       },
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: 'Lee Robinson',
+    title: 'Jin Kai Teh',
     card: 'summary_large_image',
   },
   icons: {
